a60b0be387672cd1853a8bc46386ce8797f28a4d
Harshit Sethi <harshitrsethi@gmail.com>
2025-12-28 17:15:35 +0530

    Added features of duck in src/main/java/com/opencode/alumxbackend/Animal/Duck.java

--- src/main/java/com/opencode/alumxbackend/Animal/Duck.java
+++ src/main/java/com/opencode/alumxbackend/Animal/Duck.java
@@ -1,4 +1,16 @@
 package com.opencode.alumxbackend.Animal;
 
 public class Duck {
+    public String makeSound() {
+        return "Quack! Quack!";
+    }
+    public String name() {
+        return "Donald";
+    }
+    public String color() {
+        return "Yellow";
+    }
+    public String food() {
+        return "Grains";
+    }
 }
